444b94fefd3f1b4673f6b59cb03871392c997f7a
annas <annasan14052003@gmail.com>
2023-10-27 16:30:41 +0300

    clear main

--- app/src/main/java/pexelsapp/pexelsapp/activities/MainActivity.kt
+++ app/src/main/java/pexelsapp/pexelsapp/activities/MainActivity.kt
@@ -1,23 +1,27 @@
 package pexelsapp.pexelsapp.activities
 
 import android.os.Bundle
-import android.util.Log
 import android.view.View
 import android.view.ViewGroup
-import android.widget.EditText
-import android.widget.ImageButton
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.setupWithNavController
 import com.google.android.material.bottomnavigation.BottomNavigationView
+import pexelsapp.pexelsapp.PhotoRepo
+import pexelsapp.pexelsapp.PhotoViewModel
+import pexelsapp.pexelsapp.PhotoViewModelFactory
 import pexelsapp.pexelsapp.R
+import pexelsapp.pexelsapp.db.PhotoDatabase
 
 class MainActivity : AppCompatActivity() {
-    private lateinit var searchText: EditText
-    private lateinit var clearButton: ImageButton
+    lateinit var viewModel: PhotoViewModel
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val photoRepo = PhotoRepo(PhotoDatabase(this))
+        val viewModelFactory = PhotoViewModelFactory(photoRepo)
+        viewModel = ViewModelProvider(this, viewModelFactory)[PhotoViewModel::class.java]
         val bottomNavigationView: BottomNavigationView = findViewById(R.id.bottom_navigation)
         val underline: View = findViewById(R.id.underline)
         val navController =
@@ -34,57 +38,5 @@ class MainActivity : AppCompatActivity() {
             true
         }//8oNMTj6gLf4bARFR36MVJxryWmSQRuIUk7y8H2L7cnkrGCCJ1erx8C5c
         bottomNavigationView.setupWithNavController(navController)
-
-        val customSearchView: View = layoutInflater.inflate(R.layout.search, null)
-        searchText = customSearchView.findViewById(R.id.search_text)
-        clearButton = customSearchView.findViewById(R.id.clear_button)
-        Log.e("", searchText.text.toString())
-
-        searchText.setOnFocusChangeListener { view, hasFocus ->
-            Log.d("sdf", searchText.text.toString())
-            if (hasFocus) {
-                Log.d("Focus", "Focus")
-                clearButton.visibility = View.VISIBLE
-            } else {
-                Log.d("Focus", "NoFocus")
-                clearButton.visibility = View.INVISIBLE
-            }
-        }
-
-//        val url = "https://api.pexels.com/v1/curated?per_page=1"
-//        val apiKey = "8oNMTj6gLf4bARFR36MVJxryWmSQRuIUk7y8H2L7cnkrGCCJ1erx8C5c"
-//        val baseUrl = "https://api.pexels.com/"
-//
-//        val retrofit = Retrofit.Builder()
-//            .baseUrl(baseUrl)
-//            .addConverterFactory(GsonConverterFactory.create())
-//            .build()
-//
-//        val apiService = retrofit.create(ApiService::class.java)
-//
-//        val call = apiService.getCuratedPhotos(apiKey, 3)
-//        call.enqueue(object : Callback<SearchResult> {
-//            override fun onResponse(call: Call<SearchResult>, response: Response<SearchResult>) {
-//                if (response.isSuccessful) {
-//                    val gson = Gson()
-//                    val post = response.body()
-//                    Log.d("Response", post.toString())
-//                    Log.d("photos", post?.photos!![0].url)
-//                    //  val res: SearchResult = gson.fromJson(post.toString(), SearchResult::class.java)
-//                    Log.d("Response", post.toString())
-//                } else {
-//                    Log.e("Error", call.toString())
-//                }
-//            }
-//
-//            override fun onFailure(call: Call<SearchResult>, t: Throwable) {
-//                Log.e("Error", t.message.toString())
-//            }
-//        })
-    }
-
-    fun clear(view: View) {
-        Log.e("", searchText.text.toString())
-        searchText.text.clear()
     }
 }
