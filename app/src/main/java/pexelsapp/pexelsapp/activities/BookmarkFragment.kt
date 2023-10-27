60950352b9e373172d5a0437df09a47de9a14624
annas <annasan14052003@gmail.com>
2023-10-27 16:33:31 +0300

    added bookmark

--- app/src/main/java/pexelsapp/pexelsapp/activities/BookmarkFragment.kt
+++ app/src/main/java/pexelsapp/pexelsapp/activities/BookmarkFragment.kt
@@ -1,23 +1,28 @@
 package pexelsapp.pexelsapp.activities
 
 import android.os.Bundle
-import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
+import android.widget.ImageView
 import androidx.fragment.app.Fragment
+import com.bumptech.glide.Glide
+import pexelsapp.pexelsapp.PhotoViewModel
 import pexelsapp.pexelsapp.R
 
 class BookmarkFragment : Fragment(R.layout.fragment_bookmark) {
+    private lateinit var viewModel: PhotoViewModel
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
     }
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_bookmark, container, false)
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        viewModel = (activity as MainActivity).viewModel
+        val image: ImageView = view.findViewById(R.id.imageView2)
+        Glide.with(this)
+//        https://www.pexels.com/photo/a-close-up-of-some-red-berries-on-a-tree-18855407/
+            .load("https://images.pexels.com/photos/18857014/pexels-photo-18857014.jpeg")
+            .into(image)
     }
 }
