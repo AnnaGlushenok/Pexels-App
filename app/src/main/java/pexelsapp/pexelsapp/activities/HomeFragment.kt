7c6c826906b3a710a07225db7b245d1287af1431
annas <annasan14052003@gmail.com>
2023-10-31 22:29:21 +0300

    add stubs

--- app/src/main/java/pexelsapp/pexelsapp/activities/HomeFragment.kt
+++ app/src/main/java/pexelsapp/pexelsapp/activities/HomeFragment.kt
@@ -11,8 +11,11 @@ import android.widget.Button
 import android.widget.EditText
 import android.widget.ImageButton
 import android.widget.LinearLayout
+import android.widget.ProgressBar
+import android.widget.TextView
 import android.widget.Toast
 import androidx.core.content.ContextCompat
+import androidx.core.widget.NestedScrollView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.recyclerview.widget.LinearLayoutManager
@@ -21,8 +24,10 @@ import kotlinx.coroutines.Job
 import kotlinx.coroutines.MainScope
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
+import pexelsapp.pexelsapp.Error
+import pexelsapp.pexelsapp.LastRequest
 import pexelsapp.pexelsapp.R
-import pexelsapp.pexelsapp.Resource
+import pexelsapp.pexelsapp.State
 import pexelsapp.pexelsapp.adapters.PhotoAdapter
 import pexelsapp.pexelsapp.viewModels.PhotoViewModel
 
@@ -34,6 +39,15 @@ class HomeFragment : Fragment(R.layout.fragment_home) {
     private lateinit var searchEditText: EditText
     private lateinit var recyclerView1: RecyclerView
     private lateinit var recyclerView2: RecyclerView
+    private lateinit var progressBar: ProgressBar
+    private lateinit var nestedScrollView: NestedScrollView
+    private lateinit var stubLayout: LinearLayout
+    private lateinit var noResultLayout: LinearLayout
+    private lateinit var noInternetLayout: LinearLayout
+    private lateinit var exploreTextButton: TextView
+    private lateinit var tryAgainTextButton: TextView
+    private lateinit var lastRequestStr: String
+    private var lastRequest: LastRequest = LastRequest.POPULAR
     private val buttons = mutableListOf<Button>()
     private var prevButton: Button? = null
 
@@ -42,11 +56,18 @@ class HomeFragment : Fragment(R.layout.fragment_home) {
         viewModel = (activity as MainActivity).viewModel
         recyclerView1 = view.findViewById(R.id.photo_listView1)
         recyclerView2 = view.findViewById(R.id.photo_listView2)
+        progressBar = view.findViewById(R.id.progressBar)
+        nestedScrollView = view.findViewById(R.id.nestedScrollView)
+        stubLayout = view.findViewById(R.id.stub_layout)
+        noResultLayout = view.findViewById(R.id.no_result)
+        noInternetLayout = view.findViewById(R.id.no_internet)
+        exploreTextButton = view.findViewById(R.id.explore_text_button)
+        tryAgainTextButton = view.findViewById(R.id.try_again_text_button)
         initButtons(view)
         setupRecycleView()
         viewModel.featuredCollections.observe(viewLifecycleOwner, Observer { resp ->
             when (resp) {
-                is Resource.Success -> {
+                is State.Success -> {
                     resp.data?.let { collResp ->
                         val featured = view.findViewById<LinearLayout>(R.id.featured)
                         val collections = collResp.collections
@@ -59,60 +80,136 @@ class HomeFragment : Fragment(R.layout.fragment_home) {
                     }
                 }
 
-                is Resource.Error -> {
-                    hideProgressBar()
+                is State.Error -> {
                     resp.message?.let { msg ->
-                        Log.e("err", msg)
-                        Toast.makeText(this.context, "Internet", Toast.LENGTH_LONG).show()
+                        val enum = try {
+                            Error.valueOf(msg)
+                        } catch (e: IllegalArgumentException) {
+                            null
+                        }
+                        when (enum) {
+                            Error.NO_INTERNET_CONNECTION -> showNoInternetStub(view)
+                            Error.NO_DATA -> {
+                                hideProgressBar()
+                                showNoDataStub()
+                            }
+
+                            null -> Log.e("err", msg)
+                        }
                     }
                 }
 
-                is Resource.Loading -> showProgressBar()
+                is State.Loading -> showProgressBar()
             }
+        })
+        viewModel.isLoadingProgressBar.observe(viewLifecycleOwner, Observer { isLoading ->
+            if (isLoading)
+                showProgressBar()
         })
         viewModel.photos.observe(viewLifecycleOwner, Observer { resp ->
             when (resp) {
-                is Resource.Success -> {
-                    hideProgressBar()
+                is State.Success -> {
+                    if (resp.message != null)
+                        Toast.makeText(
+                            view.context,
+                            "No internet connection, using cache",
+                            Toast.LENGTH_LONG
+                        ).show()
                     resp.data?.let { photosResp ->
                         val p = photosResp.photos
                         photoAdapter1.differ.submitList(p.subList(0, p.size / 2))
                         photoAdapter2.differ.submitList(p.subList(p.size / 2, p.size))
                     }
+                    showPage()
+                    hideProgressBar()
                 }
 
-                is Resource.Error -> {
-                    hideProgressBar()
+                is State.Error -> {
                     resp.message?.let { msg ->
-                        Log.e("err", msg)
-                        Toast.makeText(this.context, "Internet", Toast.LENGTH_LONG).show()
+                        val enum = try {
+                            Error.valueOf(msg)
+                        } catch (e: IllegalArgumentException) {
+                            null
+                        }
+                        when (enum) {
+                            Error.NO_INTERNET_CONNECTION -> showNoInternetStub(view)
+                            Error.NO_DATA -> {
+                                hideProgressBar()
+                                showNoDataStub()
+                            }
+
+                            null -> Log.e("err", msg)
+                        }
                     }
                 }
 
-                is Resource.Loading -> showProgressBar()
+                is State.Loading -> showProgressBar()
             }
         })
         viewModel.searchPhotos.observe(viewLifecycleOwner, Observer { resp ->
             when (resp) {
-                is Resource.Success -> {
-                    hideProgressBar()
+                is State.Success -> {
+                    if (resp.message != null)
+                        Toast.makeText(
+                            view.context,
+                            "No internet connection, using cache",
+                            Toast.LENGTH_LONG
+                        ).show()
                     resp.data?.let { photosResp ->
                         val p = photosResp.photos
                         photoAdapter1.differ.submitList(p.subList(0, p.size / 2))
                         photoAdapter2.differ.submitList(p.subList(p.size / 2, p.size))
                     }
+                    showPage()
+                    hideProgressBar()
                 }
 
-                is Resource.Error -> {
-                    hideProgressBar()
+                is State.Error -> {
                     resp.message?.let { msg ->
-                        Log.e("err", msg)
+                        val enum = try {
+                            Error.valueOf(msg)
+                        } catch (e: IllegalArgumentException) {
+                            null
+                        }
+                        when (enum) {
+                            Error.NO_INTERNET_CONNECTION -> showNoInternetStub(view)
+                            Error.NO_DATA -> {
+                                hideProgressBar()
+                                showNoDataStub()
+                            }
+
+                            null -> Log.e("err", msg)
+                        }
                     }
                 }
 
-                is Resource.Loading -> showProgressBar()
+                is State.Loading -> showProgressBar()
             }
         })
+    }
+
+    private fun showNoInternetStub(view: View) {
+        nestedScrollView.visibility = View.INVISIBLE
+        stubLayout.visibility = View.VISIBLE
+        noInternetLayout.visibility = View.VISIBLE
+        noResultLayout.visibility = View.GONE
+        Toast.makeText(
+            view.context,
+            "No internet connection",
+            Toast.LENGTH_LONG
+        ).show()
+    }
+
+    private fun showNoDataStub() {
+        nestedScrollView.visibility = View.INVISIBLE
+        stubLayout.visibility = View.VISIBLE
+        noResultLayout.visibility = View.VISIBLE
+        noInternetLayout.visibility = View.GONE
+    }
+
+    private fun showPage() {
+        stubLayout.visibility = View.GONE
+        nestedScrollView.visibility = View.VISIBLE
     }
 
     private fun createButton(view: View, container: LinearLayout, name: String) {
@@ -126,6 +223,7 @@ class HomeFragment : Fragment(R.layout.fragment_home) {
         button.setBackgroundResource(R.drawable.feature_button_inactive)
         button.isAllCaps = false
         button.setOnClickListener {
+            lastRequest = LastRequest.FEATURED_BUTTON
             button.setBackgroundResource(R.drawable.feature_button_active)
             button.setTextColor(
                 ContextCompat.getColor(
@@ -133,6 +231,7 @@ class HomeFragment : Fragment(R.layout.fragment_home) {
                     R.color.light_background
                 )
             )
+            lastRequestStr = button.text.toString()
             if (prevButton != null) {
                 prevButton?.setBackgroundResource(R.drawable.feature_button_inactive)
                 prevButton?.setTextColor(
@@ -142,10 +241,9 @@ class HomeFragment : Fragment(R.layout.fragment_home) {
                     )
                 )
             }
-            viewModel.searchPhotos(button.text.toString())
+            searchEditText.setText(button.text.toString())
             prevButton = button
         }
-
         container.addView(button)
         buttons.add(button)
     }
@@ -153,7 +251,6 @@ class HomeFragment : Fragment(R.layout.fragment_home) {
     private fun setupRecycleView() {
         photoAdapter1 = PhotoAdapter()
         photoAdapter2 = PhotoAdapter()
-
         photoAdapter1.setOnItemClickListener {
             startActivity(Intent(this.context, PictureActivity::class.java).apply {
                 putExtra("photo", it)
@@ -175,13 +272,13 @@ class HomeFragment : Fragment(R.layout.fragment_home) {
     }
 
     private fun hideProgressBar() {
-        Log.e("", "hide")
-//        paginationProgressBar.visibility = View.INVISIBLE
+        progressBar.visibility = View.INVISIBLE
+        nestedScrollView.visibility = View.VISIBLE
     }
 
     private fun showProgressBar() {
-        Log.e("", "show")
-//        paginationProgressBar.visibility = View.VISIBLE
+        progressBar.visibility = View.VISIBLE
+        nestedScrollView.visibility = View.INVISIBLE
     }
 
     private fun initButtons(view: View) {
@@ -195,17 +292,44 @@ class HomeFragment : Fragment(R.layout.fragment_home) {
 
             override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
                 clearButton.visibility = View.VISIBLE
+                lastRequest = LastRequest.SEARCH
                 if (s?.isEmpty() == true) {
                     clearButton.visibility = View.INVISIBLE
                     viewModel.getPhotos()
+                    prevButton?.setBackgroundResource(R.drawable.feature_button_inactive)
+                    prevButton?.setTextColor(
+                        ContextCompat.getColor(
+                            view.context,
+                            R.color.light_text
+                        )
+                    )
                 }
                 job?.cancel()
                 job = MainScope().launch {
                     delay(1000)
                     s?.let {
-                        val str = s.toString()
-                        if (str.isNotEmpty())
-                            viewModel.searchPhotos(str)
+                        lastRequestStr = s.toString()
+                        if (lastRequestStr.isNotEmpty())
+                            viewModel.searchPhotos(lastRequestStr)
+                        val button = buttons.find { b -> b.text == lastRequestStr }
+                        if (button != null) {
+                            button.setBackgroundResource(R.drawable.feature_button_active)
+                            button.setTextColor(
+                                ContextCompat.getColor(
+                                    view.context,
+                                    R.color.light_background
+                                )
+                            )
+                            prevButton = button
+                        } else {
+                            prevButton?.setBackgroundResource(R.drawable.feature_button_inactive)
+                            prevButton?.setTextColor(
+                                ContextCompat.getColor(
+                                    view.context,
+                                    R.color.light_text
+                                )
+                            )
+                        }
                     }
                 }
             }
@@ -214,5 +338,19 @@ class HomeFragment : Fragment(R.layout.fragment_home) {
         clearButton.setOnClickListener(View.OnClickListener {
             searchEditText.text.clear()
         })
+
+        exploreTextButton.setOnClickListener {
+            viewModel.getPhotos()
+            searchEditText.text.clear()
+        }
+
+        tryAgainTextButton.setOnClickListener {
+            when (lastRequest) {
+                LastRequest.POPULAR -> viewModel.getPhotos()
+                LastRequest.SEARCH -> viewModel.searchPhotos(lastRequestStr)
+                LastRequest.FEATURED_BUTTON -> viewModel.searchPhotos(lastRequestStr)
+            }
+            viewModel.getFeaturedCollections()
+        }
     }
 }
